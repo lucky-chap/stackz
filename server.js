e7f26c29bcbf3b6bee5fd3a119811a692ac315fb
warty-warthog <wartywarthog7@gmail.com>
2020-07-27 16:17:08 -0400

    Added code to connect to database

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+// Connect database
+connectDB();
 
 app.get('/', (req,res) => res.send('Api Running'));
+
+
 
 const PORT = process.env.PORT || 5000;
 
