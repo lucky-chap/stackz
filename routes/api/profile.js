78a3de06c55c37d87ef9fbed8afd4e186b8fae27
warty-warthog <wartywarthog7@gmail.com>
2020-07-29 00:40:19 -0400

    Added Github api key to config/default.json, and created route for getting github repos of a particular user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
+const request = require('request');
+const config = require('config');
 
 // @Route   GET api/profile/me
 // @desc    Get current user's profile
@@ -282,6 +284,32 @@ Router.delete('/education/:exp_id', auth, async (req,res) => {
   }
 })
 
+
+
+// @Route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+Router.get('/github/:username', (req,res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientID')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    }
+    // Using the request package here
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' })
+      }
+      res.json(JSON.parse(body));
+    })
+
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send('Server Error');
+  }
+})
 
 
 
