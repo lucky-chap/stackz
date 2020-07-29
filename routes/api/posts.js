b74f4d4c6335146f83ac5ed738079733719170d0
warty-warthog <wartywarthog7@gmail.com>
2020-07-29 03:42:35 -0400

    Added routes to get all posts, get a post by id, and delete a post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,7 +35,70 @@ Router.post('/', [auth, [
     res.status(500).send('Server Error');
   }
 
+});
 
+
+// @Route   GET api/posts
+// @desc    Get all post
+// @access  Private
+Router.get('/', auth, async (req,res) => {
+  try {
+    const posts = await Posts.find().sort({ date: -1 });
+    return res.json(posts);
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+// @Route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Private
+Router.get('/:id', auth, async (req,res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    return res.json(post);
+  } catch (e) {
+    console.error(e.message);
+    if (e.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+})
+
+
+
+// @Route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+Router.delete('/:id', auth, async (req,res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    // Check if there is no such post
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    // Check if user owns the post
+    // toString() was used because post.user in an ObjectId, not a string
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
+
+    await post.remove();
+    return res.json({ msg: 'Post removed' });
+
+  } catch (e) {
+    console.error(e.message);
+    if (e.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
 });
 
 
