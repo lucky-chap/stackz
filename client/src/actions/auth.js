adcb1c6a685b43df5bbba83886804160ed1834da
warty-warthog <wartywarthog7@gmail.com>
2020-08-02 19:38:38 +0000

    Added logout functionality

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_FAIL,
-    LOGIN_SUCCESS
+    LOGIN_SUCCESS,
+    LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -102,3 +103,11 @@ export const login = ( email, password ) => async dispatch => {
         })
     }
 } 
+
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+    dispatch({
+        type: LOGOUT
+    })
+}
