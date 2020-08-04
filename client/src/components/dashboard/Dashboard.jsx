3204f7828e1b03b660eb5fd98f9cd2f48cd93d19
Quavo Huncho <sage@localhost.localdomain>
2020-08-04 17:39:47 +0000

    Added Experience and Education components to list experience and education of the authenticated user

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -5,6 +5,8 @@ import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
 import DashboardActions from './DashboardActions';
+import Experience from './Experence';
+import Education from './Education';
 
 const Dashboard = ({
   auth: { user },
@@ -26,6 +28,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
