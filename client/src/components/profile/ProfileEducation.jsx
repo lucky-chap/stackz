dbf75845934731bf18e36ad98b381ca37f380ed0
Quavo Huncho <sage@localhost.localdomain>
2020-08-07 23:04:46 +0000

    Added functionality to display a developer's github repos, if any, show number of forks, stars and watchers of each repo, and redirected user to location of repo

--- client/src/components/profile/ProfileEducation.jsx
+++ client/src/components/profile/ProfileEducation.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import Moment from "react-moment";
 
 const ProfileEducation = ({
-  eduaction: { school, degree, fieldofstudy, current, to, from, description },
+  education: { school, degree, fieldofstudy, to, from, description },
 }) => {
   return (
     <div>
