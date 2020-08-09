37bdeba5b07baf81e2d8d3a11d8a20166561af60
Quavo Huncho <sage@localhost.localdomain>
2020-08-09 13:38:05 +0000

    Added functionality to view github repos of developers if they have any

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-module.exports = function(req,res,next) {
+module.exports = function (req, res, next) {
   // Get token from header
-  const token = req.header('x-auth-token');
+  const token = req.header("x-auth-token");
   // Check if no token
   if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
+    return res.status(401).json({ msg: "No token, authorization denied" });
   }
   // Verify Token
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
     // decoded.user refers to the payload property, 'user'
     req.user = decoded.user;
-    next()
+    next();
   } catch (e) {
-    res.staus(401).json({ msg: 'Token is not valid' })
+    res.status(401).json({ msg: "Token is not valid" });
   }
 };
