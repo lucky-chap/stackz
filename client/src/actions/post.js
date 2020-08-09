0f42af019f60ecdc77108eab4bca249f9467d733
Quavo Huncho <sage@localhost.localdomain>
2020-08-09 15:28:16 +0000

    Added functionality to like and unlike a post

---
+++ client/src/actions/post.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { setAlert } from "./alert";
+
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+
+// Get all posts
+export const getPosts = () => async (dispatch) => {
+  try {
+    const res = await axios.get("/api/posts");
+
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add Like
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: {
+        postId,
+        likes: res.data,
+      },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add Like
+export const removeLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: {
+        postId,
+        likes: res.data,
+      },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
