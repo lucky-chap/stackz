8b4d814de708baddf48abc003d1634f0dde0d3f3
warty-warthog <wartywarthog7@gmail.com>
2020-08-21 10:50:53 +0000

    Prepared for deployment

--- server.js
+++ server.js
@@ -2,18 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 // Connect database
 connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => res.send("Api Running"));
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  // Serve the index.html in build folder
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
